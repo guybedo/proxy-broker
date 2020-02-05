2b4b35d8764accf6ed220dea7da1d1a6f3eab4be
julien roch <rochjulien@yahoo.fr>
2020-02-05 09:42:36 -1000

    proxy checker test

--- proxy-broker/src/main/java/com/akalea/proxy/proxybroker/service/ProxyChecker.java
+++ proxy-broker/src/main/java/com/akalea/proxy/proxybroker/service/ProxyChecker.java
@@ -1,13 +1,18 @@
 package com.akalea.proxy.proxybroker.service;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
@@ -17,16 +22,21 @@ import org.slf4j.LoggerFactory;
 import com.akalea.proxy.proxybroker.domain.Proxy;
 import com.akalea.proxy.proxybroker.domain.ProxyQuery;
 import com.akalea.proxy.proxybroker.domain.ProxyStatus;
+import com.akalea.proxy.proxybroker.domain.ProxyValidator;
 import com.akalea.proxy.proxybroker.domain.configuration.ProxyConfiguration;
 import com.akalea.proxy.proxybroker.domain.configuration.ProxyProperties;
+import com.akalea.proxy.proxybroker.domain.configuration.ProxyProperties.ProxyCheckPolicy;
 import com.akalea.proxy.proxybroker.utils.ThreadUtils;
+import com.google.common.collect.Lists;
+
+import kong.unirest.Unirest;
 
 public class ProxyChecker {
 
     private final static Logger logger = LoggerFactory.getLogger(ProxyChecker.class);
 
-    private Executor proxyCheckExecutor;
-    private Executor validationRunExecutor;
+    private ExecutorService checkExecutor;
+    private ExecutorService validationRunExecutor;
 
     private ProxyProperties properties;
 
@@ -45,23 +55,20 @@ public class ProxyChecker {
     public ProxyChecker(ProxyProperties properties, ProxyBroker broker) {
         this.properties = properties;
         this.broker = broker;
-        proxyCheckExecutor =
-            Executors.newFixedThreadPool(
-                this.properties
-                    .getProxy()
-                    .getProxies()
-                    .getCheck()
-                    .getValidationMaxConnectionsCount());
+    }
+
+    public void start() {
+        int connectionCount =
+            this.properties
+                .getProxy()
+                .getProxies()
+                .getCheck()
+                .getValidationMaxConnectionsCount();
+        checkExecutor = Executors.newFixedThreadPool(connectionCount);
         IntStream
-            .range(
-                0,
-                this.properties
-                    .getProxy()
-                    .getProxies()
-                    .getCheck()
-                    .getValidationMaxConnectionsCount())
+            .range(0, connectionCount)
             .mapToObj(i -> proxyValidator())
-            .forEach(v -> proxyCheckExecutor.execute(v));
+            .forEach(v -> checkExecutor.execute(v));
 
         if (isValidationRunsEnabled()) {
             validationRunExecutor = Executors.newFixedThreadPool(1);
@@ -99,18 +106,54 @@ public class ProxyChecker {
     private Runnable proxyValidator() {
         return () -> {
             while (isEnabled) {
-                String uuid = this.checking.poll();
-                checkProxy(this.checkingTasks.remove(uuid));
+                try {
+                    String uuid = this.checking.poll(1, TimeUnit.DAYS);
+                    checkProxy(this.checkingTasks.remove(uuid));
+                } catch (Exception e) {
+                    logger.error("Error processing check task", e);
+                }
             }
         };
     }
 
-    private void checkProxy(ProxyCheckTask task) {
+    public void checkProxy(ProxyCheckTask task) {
         Proxy proxy = task.getProxy();
         logger.debug(String.format("Checking proxy %s", proxy.getUrl()));
         proxy.setLastCheckDate(LocalDateTime.now());
-        proxy.setLastCheck(ProxyStatus.ok);
+
+        List<ProxyValidator> validators = Lists.newArrayList(getProxyCheckPolicy().getValidators());
+        int requiredValidationCount =
+            java.lang.Math.min(
+                getProxyCheckPolicy().getValidationMinValidatorsCount(),
+                validators.size());
+        Collections.shuffle(validators);
+        long validatedCount =
+            validators
+                .subList(0, requiredValidationCount)
+                .stream()
+                .map(v -> isRequestProxyingOk(task.getProxy(), v.getUrl()))
+                .filter(r -> r)
+                .count();
+
+        proxy.setLastCheck(
+            validatedCount >= requiredValidationCount ? ProxyStatus.ok : ProxyStatus.ko);
         task.getHandler().accept(proxy);
+    }
+
+    private boolean isRequestProxyingOk(Proxy proxy, String targetUrl) {
+        try {
+            URL proxyUrl = new URL(proxy.getUrl());
+            String proxyHost = proxyUrl.getHost();
+            int proxyPort = proxyUrl.getPort() > 0 ? proxyUrl.getPort() : 80;
+            return Unirest
+                .get(targetUrl)
+                .proxy(proxyHost, proxyPort)
+                .asString()
+                .isSuccess();
+        } catch (Exception e) {
+            logger.debug(String.format("Proxying error w/ proxy %s", proxy.getUrl()), e);
+            return false;
+        }
     }
 
     private Runnable validationRun() {
@@ -127,23 +170,21 @@ public class ProxyChecker {
     private int getValidationRunDelay() {
         return Optional
             .ofNullable(
-                this.properties
-                    .getProxy()
-                    .getProxies()
-                    .getCheck()
+                getProxyCheckPolicy()
                     .getTimeBetweenValidationRunsSeconds())
             .orElse(10 * 60);
     }
 
     private boolean isValidationRunsEnabled() {
-        return this.properties
-            .getProxy()
-            .getProxies()
-            .getCheck()
+        return getProxyCheckPolicy()
             .getTimeBetweenValidationRunsSeconds() > 0;
     }
 
-    private static class ProxyCheckTask {
+    private ProxyCheckPolicy getProxyCheckPolicy() {
+        return this.properties.getProxy().getProxies().getCheck();
+    }
+
+    public static class ProxyCheckTask {
         private Proxy           proxy;
         private Consumer<Proxy> handler;
 
---
+++ proxy-broker/src/test/java/com/akalea/proxy/proxybroker/test/checker/ProxyCheckerTest.java
@@ -0,0 +1,104 @@
+package com.akalea.proxy.proxybroker.test.checker;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.akalea.proxy.proxybroker.domain.Proxy;
+import com.akalea.proxy.proxybroker.domain.ProxyStatus;
+import com.akalea.proxy.proxybroker.domain.configuration.ProxyConfiguration;
+import com.akalea.proxy.proxybroker.repository.ProxyProviderRepository;
+import com.akalea.proxy.proxybroker.service.ProxyBroker;
+import com.akalea.proxy.proxybroker.service.ProxyChecker;
+import com.akalea.proxy.proxybroker.service.ProxyChecker.ProxyCheckTask;
+
+import junit.framework.TestCase;
+import kong.unirest.GetRequest;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Unirest.class)
+public class ProxyCheckerTest extends TestCase {
+
+    @Test
+    public void test() throws IOException {
+        ProxyProviderRepository repo = new ProxyProviderRepository();
+
+        PowerMockito.mockStatic(Unirest.class);
+
+        GetRequest ok = Mockito.mock(GetRequest.class);
+        GetRequest proxy = Mockito.mock(GetRequest.class);
+        Mockito
+            .when(ok.proxy(ArgumentMatchers.any(), ArgumentMatchers.anyInt()))
+            .thenReturn(proxy);
+        HttpResponse okResponse = Mockito.mock(HttpResponse.class);
+        Mockito.when(okResponse.isSuccess()).thenReturn(true);
+        Mockito.when(okResponse.getBody()).thenReturn("");
+        Mockito.when(proxy.asString()).thenReturn(okResponse);
+
+        List<String> validationUrls =
+            ProxyConfiguration
+                .proxyProperties()
+                .getProxy()
+                .getProxies()
+                .getCheck()
+                .getValidators()
+                .stream()
+                .map(v -> v.getUrl())
+                .collect(Collectors.toList());
+        validationUrls
+            .stream()
+            .forEach(
+                url -> {
+                    Mockito
+                        .when(Unirest.get(ArgumentMatchers.eq(url)))
+                        .thenReturn(ok);
+                });
+
+        ProxyChecker checker = new ProxyChecker(new ProxyBroker());
+        Map<String, Proxy> checked = new HashMap<String, Proxy>();
+        Consumer<Proxy> handler = (p) -> checked.put(p.getUrl(), p);
+        ProxyCheckTask task =
+            new ProxyCheckTask()
+                .setProxy(new Proxy().setUrl("http://test.com"))
+                .setHandler(handler);
+        checker.checkProxy(task);
+        Assert.assertEquals(ProxyStatus.ok, task.getProxy().getLastCheck());
+        Assert.assertTrue(checked.containsKey(task.getProxy().getUrl()));
+
+        // test ko
+        GetRequest ko = Mockito.mock(GetRequest.class);
+        GetRequest proxyKo = Mockito.mock(GetRequest.class);
+        Mockito
+            .when(ko.proxy(ArgumentMatchers.any(), ArgumentMatchers.anyInt()))
+            .thenReturn(proxyKo);
+        HttpResponse koResponse = Mockito.mock(HttpResponse.class);
+        Mockito.when(koResponse.isSuccess()).thenReturn(false);
+        Mockito.when(koResponse.getBody()).thenReturn("");
+        Mockito.when(proxyKo.asString()).thenReturn(koResponse);
+        validationUrls
+            .stream()
+            .forEach(
+                url -> {
+                    Mockito
+                        .when(Unirest.get(ArgumentMatchers.eq(url)))
+                        .thenReturn(ko);
+                });
+        checker.checkProxy(task);
+        Assert.assertEquals(ProxyStatus.ko, task.getProxy().getLastCheck());
+    }
+
+}
